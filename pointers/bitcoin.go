7e5c857370a969703ef70b47eadbfb7583760b15
vishnu <vishnu14july@gmail.com>
2024-02-03 21:08:04 +0530

    pointers

---
+++ pointers/bitcoin.go
@@ -0,0 +1,14 @@
+package pointers
+
+
+type Wallet struct{
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
